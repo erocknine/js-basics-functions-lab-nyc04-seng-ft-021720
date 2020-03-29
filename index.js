774c59197cdd9d287e3c7391a60d9c12f34178af
Sir Eric <echeungsk@gmail.com>
2020-03-29 01:39:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(pickup){
   let distance = 42 - pickup
-  
   return Math.abs(distance)
 }
 
 function distanceTravelledInFeet(start, finish) {
-  let blocks = finish - start
-  return distanceInFeet = Math.abs(blocks) * 264
+  let blocks = finish - start;
+  distanceInFeet = Math.abs(blocks) * 264;
+  return distanceInFeet
 }
